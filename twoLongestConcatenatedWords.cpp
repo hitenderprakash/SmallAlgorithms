ed290828879706729d028ca5e79f988605c0637c
Hitender Prakash <hitenderprakash@gmail.com>
2017-04-10 00:05:52 -0400

    modified the two longest concatenated words algo - deleted two undesired calls and appropriate comments to explain the reason

--- twoLongestConcatenatedWords.cpp
+++ twoLongestConcatenatedWords.cpp
@@ -18,7 +18,7 @@ struct compare{
 int main()
 {
     unordered_set<string> words;
-	ifstream fobject("E:/CPP/Mad/b.txt", ifstream::in);
+	ifstream fobject("E:/CPP/Mad/words.txt", ifstream::in);
     istream_iterator<string> fitr(fobject);
     int count = 0;
     //read file
@@ -37,7 +37,9 @@ int main()
     string firstLongest="";
     string secondLongest="";
     for(int i=0; i<v.size();i++){
-        words.erase(v[i]);//delete so that it is not found in the direct search
+       // words.erase(v[i]);//delete so that it is not found in the direct search
+       //no need to delete it if isConcatenatedWord method can itself checks for first time call when each word would be found in hashmap
+       // one way is to pass the original word's size in method and make sure that this entire word match does not cause true return
         if(isConcatenatedWord(v[i],words,v[i].size() )){
             if(firstLongest==""){
                 firstLongest=v[i];
@@ -47,19 +49,19 @@ int main()
                 break;
             }
         }
-        words.insert(v[i]);//restore the original hashmap
+       // words.insert(v[i]);//restore the original hashmap
     }
-    cout<<"\n First Longest concatenated word: "<<firstLongest;
-    cout<<"\n Second Longest concatenated word: "<<secondLongest;
+    cout<<"\nFirst Longest concatenated word: "<<firstLongest;
+    cout<<"\nSecond Longest concatenated word: "<<secondLongest;
     return 0;
 }
 bool isConcatenatedWord(string word,unordered_set<string> &se,int len){
     if(word.size()==0){
         return false;
     }
-    if(isWordInCollection(word,se) && word.size()<len){
-        return true;
-    }
+    if(isWordInCollection(word,se) && word.size()<len){ //word size is matched with the size of original word
+        return true;                                    //Just to ensure that the original word was not directly found in hashmap
+    }                                                   // but some suffix was found. Otherwise on first call every word would return true
     for (int i = 1; i < word.length(); i++) {
         string prefix = word.substr(0, i);
         string remainder = word.substr(i, word.length());
