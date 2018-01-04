9f2cfdc75a15cd2f83268fe7e50c737269bea9f9
hprakash <hprakash@iu.edu>
2018-01-04 15:15:51 -0500

    modified the Sufix Table creation code

--- SufixTableForPatternMatching.cpp
+++ SufixTableForPatternMatching.cpp
@@ -1,51 +1,27 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-void SetTableValue(string input, int *SufixTable, int i, int k){
-
-    if(k==0){
-        if (input[i]==input[0]){
-            SufixTable[i]=1;
-        }
-        else{
-            SufixTable[i]=0;
-        }
-    }
-    else if(input[i]==input[(SufixTable[k])]){
-        SufixTable[i]=SufixTable[k]+1;
-    }
-    else if((input[i]!=input[(SufixTable[k])])&&(k==SufixTable[k])){
-            SufixTable[i]=0;
-            //if k=sufixTable[k] then program will go to infinite loop or crash
-    }
-    else{
-        SetTableValue(input,SufixTable, i, SufixTable[k]);
-    }
-
-}
-
-void CreateSufixTable(string input, int *SufixTable){
-    int len=input.length();
-    SufixTable[0]=0;
-
-    for(int i=1;i<len;i++){
-        SetTableValue(input,SufixTable, i, i-1);
-    }
+void KMP(string &s,vector<int> &res){
+	for(int i=1;i<s.size();i++){
+		int j=res[i-1];
+		while(j>0 && s[i]!=s[j]){j=res[j-1];}
+		res[i]=j+(s[i]==s[j]);
+	}
 }
 
 int main()
 {
-    string A="AACDEACAAAACDEACAAAA";//"A";//"AACDEACAAAACDEACAAAA";//"AABAABAB";
-    int *ST;
-    ST=new int [A.length()];
-    CreateSufixTable(A,ST);
-    cout<<"\nInput String: "<<A;
+    string test="AACDEACAAAACDEACAAAA";//"A";//"AACDEACAAAACDEACAAAA";//"AABAABAB";
+    vector<int> tab(test.size(),0);
+	KMP(test,tab);
+    cout<<"\nInput String: "<<test;
     cout<<"\nSufix Table: "<<"[";
-    for(int i=0;i<A.length();i++){
-        cout<<" "<<ST[i];
+    for(int i=0;i<tab.size();i++){
+        cout<<tab[i]<<"  ";
     }
-    cout<<"]";
+    cout<<"\b\b]\n";
     return 0;
 }
